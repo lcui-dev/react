efb4ea456175964ed21eba800c889f51fe6816b5
Liu <lc-soft@live.cn>
2024-10-19 14:43:26 +0800

    fix: fmt() 函数生成的代码缺失必要头文件

--- src/fmt.ts
+++ src/fmt.ts
@@ -2,11 +2,13 @@ import {
   CType,
   Value,
   factory,
+  getComponentContext,
   getFunctionContext,
   stringifyValue,
 } from "./binding.js";
 
 export default function fmt(...args: Value[]) {
+  const component = getComponentContext();
   const ctx = getFunctionContext();
   const str = factory.createStringVariable();
   const len = factory.createNumericVariable(
@@ -16,6 +18,9 @@ export default function fmt(...args: Value[]) {
   );
   const list = args.map((value) => stringifyValue(value).__meta__.name);
 
+  component.headerFiles.add('<stdlib.h>');
+  component.headerFiles.add('<stdio.h>');
+  component.headerFiles.add('<string.h>');
   ctx.body.push(...list.map((id) => `${len.__meta__.name} += strlen(${id})`));
   ctx.body.push(
     `${str.__meta__.name} = malloc(sizeof(char) * ${len.__meta__.name})`
